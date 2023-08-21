36db1c9276efeb61b8d7337e923c5d75c671c980
Ihor Marchuk <usdreldo@gmai.com>
2023-08-21 19:21:28 +0200

    comments removed, function for tab swithing added

--- js/todoapp.js
+++ js/todoapp.js
@@ -20,6 +20,29 @@ const demoTodo = [
   },
 ];
 
+function todosVisibility(
+  visibility,
+  display1,
+  display2,
+  isShowAll,
+  isShowActive
+) {
+  trashCans.forEach((e) => (e.style.visibility = visibility));
+  deleteCompletedDiv.style.display = display1;
+  taskAddDiv.style.display = display2;
+  for (let todo of allTodos) {
+    if (isShowAll) {
+      todo.style.display = "block";
+    } else if (isShowActive) {
+      todo.style.display =
+        todo.getAttribute("completed") === "true" ? "none" : "block";
+    } else {
+      todo.style.display =
+        todo.getAttribute("completed") === "true" ? "block" : "none";
+    }
+  }
+}
+
 //**Todo creation**
 const todos = document.createElement("ul");
 todos.className = "todo-ul";
@@ -63,7 +86,6 @@ addButton.addEventListener("click", () => {
 
     tasks.push(userInput);
     storage.set("tasks", tasks);
-    // localStorage.setItem("tasks", JSON.stringify(tasks));
 
     addTodo(userInput);
     input.value = "";
@@ -73,7 +95,6 @@ addButton.addEventListener("click", () => {
   }
 });
 
-//**Todo completing**
 let allCheckBoxes = document.querySelectorAll(".check");
 function completeTodo() {
   for (let checkbox of allCheckBoxes) {
@@ -105,53 +126,28 @@ function completeTodo() {
 
 completeTodo();
 
-//**Switching between tabs**
 const showAll = document.querySelector("#all-button");
 const showActive = document.querySelector("#active-button");
 const showCompleted = document.querySelector("#completed-button");
-let allTodos = document.querySelectorAll(".todo-ul-item");
-let trashCans = document.querySelectorAll(".completed-trash");
 const filterBtnContainer = document.querySelectorAll(".tab-links");
 const taskAddDiv = document.querySelector(".task-adding");
 const deleteCompletedDiv = document.querySelector(".delete-completed");
+let allTodos = document.querySelectorAll(".todo-ul-item");
+let trashCans = document.querySelectorAll(".completed-trash");
 
 showAll.addEventListener("click", () => {
-  allTodos = document.querySelectorAll(".todo-ul-item");
-  trashCans = document.querySelectorAll(".completed-trash");
-  trashCans.forEach((e) => (e.style.visibility = "hidden"));
-  deleteCompletedDiv.style.display = "none";
-  taskAddDiv.style.display = "flex";
-  for (let todo of allTodos) {
-    todo.style.display = "block";
-  }
+  todosVisibility("hidden", "none", "flex", true, false);
 });
 31;
 showActive.addEventListener("click", () => {
-  allTodos = document.querySelectorAll(".todo-ul-item");
-  trashCans = document.querySelectorAll(".completed-trash");
-  trashCans.forEach((e) => (e.style.visibility = "hidden"));
-  deleteCompletedDiv.style.display = "none";
-  taskAddDiv.style.display = "flex";
-  for (let todo of allTodos) {
-    todo.style.display =
-      todo.getAttribute("completed") === "true" ? "none" : "block";
-  }
+  todosVisibility("hidden", "none", "flex", false, true);
 });
 
 showCompleted.addEventListener("click", () => {
-  allTodos = document.querySelectorAll(".todo-ul-item");
-  trashCans = document.querySelectorAll(".completed-trash");
-  trashCans.forEach((e) => (e.style.visibility = "visible"));
-  deleteCompletedDiv.style.display = "flex";
-  taskAddDiv.style.display = "none";
-  for (let todo of allTodos) {
-    todo.style.display =
-      todo.getAttribute("completed") === "true" ? "block" : "none";
-  }
+  todosVisibility("visible", "flex", "none", false, false);
   completeTodo();
 });
 
-//**Deleting todos**
 todos.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-trash-can")) {
     const currentTodo = e.target.parentElement.parentElement;
@@ -166,7 +162,6 @@ todos.addEventListener("click", (e) => {
   }
 });
 
-//**Delete completed todos**
 deleteCompletedDiv.addEventListener("click", () => {
   allTodos = document.querySelectorAll(".todo-ul-item");
   allTodos.forEach((todo) => {
@@ -178,7 +173,6 @@ deleteCompletedDiv.addEventListener("click", () => {
   storage.set("tasks", tasks);
 });
 
-//**Active tab indicator**
 const tabs = document.querySelectorAll(".nav-section");
 tabs.forEach((link) => {
   link.addEventListener("click", () => {
