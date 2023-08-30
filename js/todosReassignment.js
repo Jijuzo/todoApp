80dea6aca1045ad615b97987b78053c999ceb3ec
Ihor Marchuk <usdreldo@gmai.com>
2023-08-30 19:26:34 +0200

    added function for reassignment of some variables

---
+++ js/todosReassignment.js
@@ -0,0 +1,6 @@
+export function todosReassignment() {
+  let allTodos = document.querySelectorAll(".todo-ul-item");
+  let allCheckBoxes = document.querySelectorAll(".check");
+  let reassignedVariables = [allTodos, allCheckBoxes];
+  return reassignedVariables;
+}
