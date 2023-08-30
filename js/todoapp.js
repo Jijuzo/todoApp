ecdf8970c395f261b15319832482306d7cc6160a
Ihor Marchuk <usdreldo@gmai.com>
2023-08-30 19:26:10 +0200

    event listener moved to another file

--- js/todoapp.js
+++ js/todoapp.js
@@ -1,34 +1,21 @@
-import storage from "./storage.js";
-import activeTab from "./tabSwitch.js";
-import completeTodo from "./completeTodo.js";
-import displayTodos from "./displayTodos.js";
-import demoTodo from "./demoTodo.js";
-import createTodo from "./createTodo.js";
-import deleteSingleTodo from "./deleteSingleTodo.js";
+import { storage } from "./storage.js";
+import { setActiveTab } from "./tabSwitch.js";
+import { initializeTodos } from "./initializeTodos.js";
+import { displayTodos } from "./displayTodos.js";
+import { demoTodo } from "./demoTodo.js";
+import { createTodo } from "./createTodo.js";
+import { deleteSingleTodoTrigger } from "./deleteSingleTodoTrigger.js";
+import { deleteCompletedTodosTrigger } from "./deleteCompletedTodosTrigger.js";
 
 const todos = document.createElement("ul");
 todos.className = "todo-ul";
 let tasks = storage.get("tasks") ?? demoTodo;
 
 displayTodos();
-
-let allTodos = document.querySelectorAll(".todo-ul-item");
-let allCheckBoxes = document.querySelectorAll(".check");
-const deleteCompletedDiv = document.querySelector(".delete-completed");
-deleteCompletedDiv.addEventListener("click", () => {
-  allTodos = document.querySelectorAll(".todo-ul-item");
-  allTodos.forEach((todo) => {
-    if (todo.dataset.completed === "true") {
-      todos.removeChild(todo);
-    }
-  });
-  tasks = tasks.filter((task) => task.completed === false);
-  storage.set("tasks", tasks);
-});
-
 createTodo();
-completeTodo();
-deleteSingleTodo();
-activeTab();
+initializeTodos();
+deleteSingleTodoTrigger();
+deleteCompletedTodosTrigger();
+setActiveTab();
 
-export { deleteCompletedDiv, allTodos, allCheckBoxes, tasks, todos };
+export { tasks, todos };
