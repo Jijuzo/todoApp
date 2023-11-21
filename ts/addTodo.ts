2be35cfc5d06727b98a2dbff5399ab7f7713a9b0
Ihor Marchuk <usdreldo@gmai.com>
2023-11-21 17:42:26 +0100

    function prop type changed

--- ts/addTodo.ts
+++ ts/addTodo.ts
@@ -1,10 +1,11 @@
 import { todos } from "./todoapp.js";
+import { Todo } from "./types.js";
 
-function addTodo(todo: { name: string; id: number; completed: boolean }) {
+function addTodo(todo: Todo) {
   const todosDiv = document.querySelector("#tab1");
   const newTodo = document.createElement("li");
   newTodo.className = "todo-ul-item";
-  newTodo.id = todo.id.toString();
+  newTodo.id = todo.id;
   newTodo.innerHTML = `
             <label class="task-label">
             <input type="checkbox" class="check"> ${todo.name}
