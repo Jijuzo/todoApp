ccfd4d643689d8ab8596ad2a79c01812467ae078
Ihor Marchuk <usdreldo@gmai.com>
2023-11-21 17:44:14 +0100

    import removed

--- ts/todoapp.ts
+++ ts/todoapp.ts
@@ -2,14 +2,14 @@ import { storage } from "./storage.js";
 import { setActiveTabListener } from "./tabSwitch.js";
 import { initializeTodos } from "./initializeTodos.js";
 import { displayTodos } from "./displayTodos.js";
-import { demoTodo, TypeTodo } from "./demoTodo.js";
+import { demoTodos } from "./demoTodos.js";
 import { setTodoCreationListener } from "./setTodoCreationListener.js";
 import { setSingleTodoDeletionListener } from "./setSingleTodoDeletionListener.js";
 import { setCompletedTodosDeletionListener } from "./setCompletedTodosDeletionListener.js";
 
 const todos = document.createElement("ul");
 todos.className = "todo-ul";
-let tasks: string | TypeTodo = storage.get("tasks") ?? demoTodo;
+let tasks = storage.get("tasks") ?? demoTodos;
 
 displayTodos();
 setTodoCreationListener();
