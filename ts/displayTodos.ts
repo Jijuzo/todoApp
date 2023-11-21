c9f3c45fad4da81d6c53dfd3c42d47504f679681
Ihor Marchuk <usdreldo@gmai.com>
2023-11-21 17:42:52 +0100

    type check removed

--- ts/displayTodos.ts
+++ ts/displayTodos.ts
@@ -1,13 +1,10 @@
 import { addTodo } from "./addTodo.js";
 import { tasks } from "./todoapp.js";
-import { isTypeTodo } from "./setSingleTodoDeletionListener.js";
 
 function displayTodos() {
-  if (isTypeTodo(tasks)) {
-    tasks.forEach((task) => {
-      addTodo(task);
-    });
-  }
+  tasks.forEach((task) => {
+    addTodo(task);
+  });
 }
 
 export { displayTodos };
